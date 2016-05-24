c21cf8d66f3e9cdf4258e87590b8dbbda2dfb830
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-05-24 17:20:20 +0100

    Adding docker compose file

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -4,12 +4,15 @@ var serveStatic = require('serve-static');
 var request = require('request');
 
 const PORT=8079;
-var catalogueUrl = "http://192.168.99.100:8081/catalogue";
+// var catalogueUrl = "http://192.168.99.100:8081/catalogue";
+var catalogueUrl = "http://catalogue:8081/catalogue";
 
 var serve = serveStatic("./");
 
 function handleRequest(req, res) {
+	console.log('request: ' + req.url)
 	if (req.url.indexOf('/getData') > -1) {
+		console.log('get data');
 		request(catalogueUrl, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 		    console.log(body);
