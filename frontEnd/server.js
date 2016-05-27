f7dc8aa1165feacdad9cd2c2f681ce2a497c2965
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-05-27 14:06:04 +0100

    Added accounts, orders, customers, carts API to frontEnd server

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -2,6 +2,7 @@ var request = require('request');
 var express = require('express');
 var path = require("path");
 var bodyParser = require("body-parser");
+var async = require("async");
 
 var app = express();
 app.use(express.static(__dirname + "/"));
@@ -9,7 +10,19 @@ app.use(bodyParser.json());
 
 var catalogueUrl = "http://catalogue/catalogue";
 var accountsUrl = "http://accounts/accounts";
-var cartsUrl = "http://carts/carts/search/findByCustomerId";
+var cartsUrl = "http://carts/carts";
+var ordersUrl = "http://orders/orders";
+var itemsUrl = "http://items/items";
+var customersUrl = "http://accounts/customers";
+
+if (app.get('env') == "development") {
+	catalogueUrl = "http://localhost:8081/catalogue";
+	accountsUrl = "http://localhost:8082/accounts";
+	cartsUrl = "http://localhost:8081/carts";
+	itemsUrl = "http://localhost:8081/items";
+	ordersUrl = "http://localhost:8083/orders";
+	customersUrl = "http://localhost:8082/customers";
+}
 
 function handleError(res, reason, message, code) {
 	console.log("Error: " + reason);
@@ -18,9 +31,6 @@ function handleError(res, reason, message, code) {
 
 // Catalogue
 app.get("/catalogue", function(req, res) {
-	if (app.get('env') == "development") {
-		catalogueUrl = "http://localhost:8081/catalogue";
-	}
 
 	request(catalogueUrl, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
@@ -33,9 +43,6 @@ app.get("/catalogue", function(req, res) {
 });
 
 app.get("/catalogue/:id", function(req, res) {
-	if (app.get('env') == "development") {
-		catalogueUrl = "http://localhost:8081/catalogue";
-	}
 
 	request(catalogueUrl + "/" + req.params.id, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
@@ -49,11 +56,8 @@ app.get("/catalogue/:id", function(req, res) {
 
 // Accounts
 app.get("/accounts/", function(req, res) {
-	if (app.get('env') == "development") {
-		accountsUrl = "http://localhost:8082/accounts";
-	}
 
-	request(accountsUrl + "?custId=fakeId", function (error, response, body) {
+	request(accountsUrl + "?custId=1", function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 		    console.log(body);
 			res.writeHeader(200);
@@ -64,9 +68,6 @@ app.get("/accounts/", function(req, res) {
 });
 
 app.get("/accounts/:id", function(req, res) {
-	if (app.get('env') == "development") {
-		accountsUrl = "http://localhost:8082/accounts";
-	}
 
 	request(accountsUrl + "/" + req.params.id, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
@@ -80,18 +81,127 @@ app.get("/accounts/:id", function(req, res) {
 
 //Carts
 app.get("/carts/", function(req, res) {
-	if (app.get('env') == "development") {
-		cartsUrl = "http://localhost:8083/carts/search/findByCustomerId";
-	}
-
-	request(accountsUrl + "?custId=fakeId", function (error, response, body) {
+	console.log("Request received: " + req.url);
+	request.get(cartsUrl + "/search/findByCustomerId?custId=1", function (error, response, body) {
+		console.log("Response received from carts.");
 		if (!error && response.statusCode == 200) {
-		    console.log(body);
+		    // console.log(body);
 			res.writeHeader(200);
 			res.write(body);
 			res.end()
 		  }
 	}.bind( {res: res} ));
+});
+
+app.get("/carts/:cartId", function(req, res) {
+	console.log("Request received: " + req.url);
+	request.get(cartsUrl + "/" + req.params.cartId, function (error, response, body) {
+		console.log("Response received from carts.");
+		if (!error && response.statusCode == 200) {
+		    // console.log(body);
+			res.writeHeader(200);
+			res.write(body);
+			res.end()
+		  } else {
+		  	console.log(error);
+			res.writeHeader(response.statusCode);
+			res.end()
+		  }
+	}.bind( {res: res} ));
+});
+
+app.post("/carts/:cartId/items", function(req, res) {
+	async.waterfall([
+		function(callback) {
+			console.log("Request received with body: " + JSON.stringify(req.body));
+			var options = {
+			  uri: itemsUrl,
+			  method: 'POST',
+			  json: true,
+			  body: req.body
+			};
+			request(options, function(error, response, body) {
+				if (error) {
+					console.log(error);
+					callback(true);
+					return;
+				}
+				console.log("Received response: " + JSON.stringify(body));
+				link = body._links.item.href;
+				callback(null, link);
+			});
+		},
+		function(arg1, callback) {
+			var options = {
+				headers: {
+					'Content-Type': 'text/uri-list'
+				},
+				uri: cartsUrl + "/" + req.params.cartId + "/items",
+				method: 'POST',
+				body: arg1
+			};
+			request(options, function(error, response, body) {
+				if (error) {
+					console.log(error);
+					callback(true);
+					return;
+				}
+				cartItem = body;
+				callback(null, cartItem);
+			});
+		}
+	],
+	function(err, result) {
+		res.writeHeader(200);
+		res.write(JSON.stringify(result));
+		res.end()
+	});
+});
+
+//Orders
+app.post("/orders", function(req, res) {
+	console.log("Request received with body: " + JSON.stringify(req.body));
+	async.waterfall([
+		function(callback) {
+			request(customersUrl + "/" + req.body.customer, function(error, response, body) {
+				if (error) {
+					console.log(error);
+					callback(true);
+					return;
+				}
+				console.log("Received response: " + JSON.stringify(body));
+				jsonBody = JSON.parse(body);
+				customerlink = jsonBody._links.customer.href;
+				addressLink = jsonBody._links.addresses.href;
+				cardLink = jsonBody._links.cards.href;
+				var order = {
+					"customer": customerlink,
+					"address": addressLink,
+					"card": cardLink,
+					"items": null
+				}
+				callback(null, order);
+			});
+		},
+		function(arg1, callback) {
+			request.get(cartsUrl + "/search/findByCustomerId?custId=1", function (error, response, body) {
+				if (error) {
+					console.log(error);
+					callback(true);
+					return;
+				}
+				console.log("Received response: " + JSON.stringify(body));
+				jsonBody = JSON.parse(body);
+				arg1.items = jsonBody._embedded.carts[0]._links.items.href;
+				callback(null, arg1);
+			});
+		}
+	],
+	function(err, result) {
+		res.writeHeader(200);
+		res.write(JSON.stringify(result));
+		res.end()
+	});
 });
 
 var server = app.listen(process.env.PORT || 8079, function () {
