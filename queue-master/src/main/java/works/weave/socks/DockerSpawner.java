d68a569e8d2cc168e31e18b99aa489c82950791d
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-06-08 12:24:37 +0100

    Remove shipping container after completion

--- queue-master/src/main/java/works/weave/socks/DockerSpawner.java
+++ queue-master/src/main/java/works/weave/socks/DockerSpawner.java
@@ -1,10 +1,14 @@
 package works.weave.socks;
 
+import java.lang.Exception;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.command.PullImageResultCallback;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
 import org.springframework.stereotype.Component;
 
 
@@ -12,9 +16,11 @@ import org.springframework.stereotype.Component;
 public class DockerSpawner {
 	
 	private DockerClient dc;
+	private ExecutorService dockerPool;
 
 	private String imageName = "alpine";
 	private String imageVersion = "3.1";
+	private int poolSize = 50;
 
 	public void init() {
 		if (dc == null) {
@@ -30,15 +36,28 @@ public class DockerSpawner {
 
             dc.pullImageCmd(imageName).withTag(imageVersion).exec(new PullImageResultCallback()).awaitSuccess();
 		}
+		if (dockerPool == null) {
+			dockerPool = Executors.newFixedThreadPool(poolSize);
+		}
 	}
 
 	public void spawn() {
-		System.out.println("Spawning new container");
-		CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withCmd("sleep", "30").exec();
+		dockerPool.execute(new Runnable() {
+		    public void run() {
+				System.out.println("Spawning new container");
+				try {
+					CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withCmd("sleep", "30").exec();
 
-		dc.startContainerCmd(container.getId()).exec();
-		// dc.stopContainerCmd(container.getId()).exec();
-		// dc.waitContainerCmd(container.getId()).exec();
-		System.out.println("Finished spawn.");
+					dc.startContainerCmd(container.getId()).exec();
+					System.out.println("Spawned container with id: " + container.getId());
+					// TODO instead of just sleeping, call await on the container and remove once it's completed.
+					Thread.sleep(45000);
+					dc.removeContainerCmd(container.getId()).exec();
+					System.out.println("Removed Container:" + container.getId());
+				} catch (Exception e) {
+					System.out.println("Exception !!");
+				}
+		    }
+		});
 	}
 }
