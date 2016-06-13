18b3e5f586f33462b1af806c76b8a9845d705ca9
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-06-13 11:59:00 +0200

    Launch new containers on backoffice network

--- queue-master/src/main/java/works/weave/socks/DockerSpawner.java
+++ queue-master/src/main/java/works/weave/socks/DockerSpawner.java
@@ -1,35 +1,43 @@
 package works.weave.socks;
 
-import java.lang.Exception;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Network;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.command.PullImageResultCallback;
 import com.github.dockerjava.core.command.ExecStartResultCallback;
 import org.springframework.stereotype.Component;
 
+import java.lang.Exception;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.List;
 
 @Component
 public class DockerSpawner {
-	
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	private DockerClient dc;
 	private ExecutorService dockerPool;
 
 	private String imageName = "alpine";
 	private String imageVersion = "3.1";
+	private String networkId = "weavedemo_backoffice";
 	private int poolSize = 50;
 
 	public void init() {
 		if (dc == null) {
 			DockerClientConfig.DockerClientConfigBuilder builder = DockerClientConfig.createDefaultConfigBuilder();
 
-            String dockerHostEnv = System.getenv("DOCKER_HOST");
-            if (dockerHostEnv == null || dockerHostEnv.trim() == "") {
-                builder.withUri("unix:///var/run/docker.sock");
-            }
+			// TODO from previous version of dockerjava
+            // String dockerHostEnv = System.getenv("DOCKER_HOST");
+            // if (dockerHostEnv == null || dockerHostEnv.trim() == "") {
+                // builder.withUri("unix:///var/run/docker.sock");
+            // }
 
             DockerClientConfig config = builder.build();
             dc = DockerClientBuilder.getInstance(config).build();
@@ -44,18 +52,18 @@ public class DockerSpawner {
 	public void spawn() {
 		dockerPool.execute(new Runnable() {
 		    public void run() {
-				System.out.println("Spawning new container");
+				logger.debug("Spawning new container");
 				try {
-					CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withCmd("sleep", "30").exec();
-
-					dc.startContainerCmd(container.getId()).exec();
-					System.out.println("Spawned container with id: " + container.getId());
+					CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withNetworkMode(networkId).withCmd("sleep", "30").exec();
+					String containerId = container.getId();
+					dc.startContainerCmd(containerId).exec();
+					logger.debug("Spawned container with id: " + container.getId() + " on network: " + networkId);
 					// TODO instead of just sleeping, call await on the container and remove once it's completed.
 					Thread.sleep(45000);
 					dc.removeContainerCmd(container.getId()).exec();
-					System.out.println("Removed Container:" + container.getId());
+					logger.debug("Removed Container:" + container.getId());
 				} catch (Exception e) {
-					System.out.println("Exception !!");
+					logger.error("Exception trying to launch new container. " + e);
 				}
 		    }
 		});
