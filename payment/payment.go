496ca2eabf10be8adbcbc6f93ffe6feb86477a1c
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-06-29 12:05:24 +0200

    Payment service to abuse CPU

--- payment/payment.go
+++ payment/payment.go
@@ -3,12 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 var port string
 
+var rando *rand.Rand
+
 func main() {
+
+	s := rand.NewSource(time.Now().UnixNano())
+    rando = rand.New(s)
 
 	flag.StringVar(&port, "port", "8082", "Port on which to run")
 	flag.Parse()
@@ -19,6 +26,25 @@ func main() {
 }
 
 func paymentAuthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Received payment auth request... Authorized.\n")
+	id := rando.Intn(10000)
+	fmt.Printf("Received payment auth request. Payment id=%d \n", id)
+	abuseCPU(10, 20, id)
 	w.WriteHeader(200)
+}
+
+func abuseCPU(n, s, id int) {
+	for i := 0; i < n; i++ {
+		go useful(s, id)
+	}
+}
+
+func useful(s, id int) {
+	fmt.Printf("payment validation attempt for id=%d\n", id)
+	end := time.Now().Add(time.Second * time.Duration(s));
+	count := 0
+	for time.Now().Before(end) {
+		count = count + 1
+		count = 0
+	}
+	fmt.Printf("Payment authorized. ID=%d\n", id)
 }
