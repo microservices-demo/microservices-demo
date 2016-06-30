414d978ad87fb10d2b14af0b2c153d5ad8be1ebb
Phil Winder <phil@winderresearch.com>
2016-06-30 17:18:11 +0100

    WIP: Added --domain options to front-end and orders.

--- install/mesos-cni/provisionWeaveCNI.sh
+++ install/mesos-cni/provisionWeaveCNI.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
+if [ -z "$1" ]; then
+    echo "Warn: No Master IP passed. Assuming this is master"
+fi
+
 sudo curl -sL git.io/weave -o /usr/local/bin/weave
 sudo chmod a+x /usr/local/bin/weave
 sudo mkdir -p /opt/cni/bin
 sudo mkdir -p /etc/cni/net.d
+sudo weave stop # Just in case it's already running.
 sudo weave setup
 sudo weave launch --no-dns $1
 sudo weave expose
@@ -17,10 +22,6 @@ sudo mkdir -p /etc/cni/net.d
 echo '/opt/cni/bin' | sudo tee /etc/mesos-slave/network_cni_plugins_dir
 echo '/etc/cni/net.d' | sudo tee /etc/mesos-slave/network_cni_config_dir
 
-# For Mesos bridge example (as described in the documentation), uncomment the following line:
-#./scripts/provisionMesosCNIExample.sh
-
-
 # WORKAROUND TO FIX WEAVE BUG: https://github.com/weaveworks/weave/issues/2394
 
 echo '#!/bin/sh
@@ -29,5 +30,3 @@ docker run --rm --privileged --net=host -v /var/run/docker.sock:/var/run/docker.
  -e CNI_IFNAME -e CNI_ARGS -e CNI_PATH -v /etc/cni:/etc/cni -v /opt/cni:/opt/cni \
  -v /run/mesos/isolators/network/cni:/run/mesos/isolators/network/cni \
  weaveworks/plugin:1.6.0  --cni-net' | sudo tee /opt/cni/bin/weave-net
-
-sudo service mesos-slave restart
