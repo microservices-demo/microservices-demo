8154a02d5d12011dd1b1af7c6bf4736716d8baf7
Phil Winder <phil@winderresearch.com>
2016-07-04 17:46:26 +0100

    Revert Shipping Rabbit change. Added simple try catch, which should allow queue failures.

--- shipping/src/main/java/works/weave/socks/RabbitMqConfiguration.java
+++ shipping/src/main/java/works/weave/socks/RabbitMqConfiguration.java
@@ -7,20 +7,16 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration
 {
-    @Value("${queue.address:rabbitmq")
-    private String queueAddress;
-
     @Bean
     public ConnectionFactory connectionFactory()
     {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(queueAddress);
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("rabbitmq");
         // CachingConnectionFactory connectionFactory = new CachingConnectionFactory("192.168.99.104");
         connectionFactory.setUsername("guest");
         connectionFactory.setPassword("guest");
