33fbb5999b82dfeaecfdff99c6ee7b58e8fd1e83
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-07-06 12:55:49 +0200

    removing unused scripts

--- deploy/kubernetes/scripts/uninstall.sh
+++
@@ -1,6 +0,0 @@
-
-# Remove all deployments, will also remove pods
-kubectl delete deployments --all
-
-# Remove all services, except kubernetes
-kubectl delete service $(kubectl get services | cut -d" " -f1 | grep -v NAME | grep -v kubernetes)
