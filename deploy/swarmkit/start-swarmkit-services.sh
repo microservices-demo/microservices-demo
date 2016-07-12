ea444a4935a4c0e570df40ba3b50f35c928bbe35
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-07-12 17:10:09 +0200

    Updating starter script

--- deploy/swarmkit/start-swarmkit-services.sh
+++ deploy/swarmkit/start-swarmkit-services.sh
@@ -20,6 +20,7 @@ cleanup_services() {
     docker service rm front-end
     docker service rm edge-router
     docker service rm catalogue
+    docker service rm weave-scope
 }
 
 if ! command_exists "docker" ; then
@@ -34,22 +35,34 @@ if [ "$1" == "cleanup" ]; then
 fi
 
 echo "Creating front-end service"
-docker service create --network ingress \
-       --name front-end --env "reschedule=on-node-failure" weaveworksdemos:front-end
+docker service create \
+       --network ingress \
+       #       --publish 8079 \
+       --name front-end --env "reschedule=on-node-failure" weaveworksdemos/front-end:latest
 
 exit_on_failure
 
 echo "Creating edge-router service"
 docker service create \
-       --name edge-router --network ingress \
-       --env "reschedule=on-node-failure" weaveworksdemos:edge-router
+       --name edge-router \
+       #       --publish 80 \
+       --network ingress \
+       --env "reschedule=on-node-failure" weaveworksdemos/edge-router:latest
 
 exit_on_failure
 
 echo "Creating catalogue service"
 docker service create \
-       --name catalogue --network ingress \
-       --env "reschedule=on-node-failure" weaveworksdemos:catalogue
+       --name catalogue \
+       --network ingress \
+       --env "reschedule=on-node-failure" weaveworksdemos/catalogue:latest
 exit_on_failure
 
-echo "Done" 
+echo "Creating weave scope service"
+docker service create \
+       --name weave-scope \
+       #       --publish 4040 \
+       --network ingress weaveworks/scope:0.15 "--probe.docker" "true"
+exit_on_failure
+
+echo "Done"
