9ed4704c43060b2a304b1009513c2dd51be3e358
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-07-13 16:56:48 +0200

    Cleanup, added usage

--- deploy/swarmkit/master1/share/start-swarmkit-services.sh
+++ deploy/swarmkit/master1/share/start-swarmkit-services.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# if [ -z ${SWARM_SOCKET} ]; then
-#     echo "SWARM_SOCKET environment variable is unset."
-#     exit 1
-# fi
+function usage() {
+    echo "usage: $(basename $0) [cleanup]"
+    echo "  When cleanup option is provided, the script will remove previously initiated services"
+}
 
 command_exists() {
     command -v "$@" > /dev/null 2>&1
@@ -18,7 +18,6 @@ exit_on_failure() {
 
 cleanup_services() {
     docker service rm front-end
-    # docker service rm edge-router
     docker service rm catalogue
     docker service rm accounts
     docker service rm accounts-db
@@ -27,11 +26,8 @@ cleanup_services() {
     docker service rm orders
     docker service rm orders-db
     docker service rm shipping
-    # docker service rm queue-master
-    # docker service rm rabbitmq
     docker service rm payment
     docker service rm login
-    #docker service rm weave-scope
 }
 
 if ! command_exists "docker" ; then
@@ -45,6 +41,11 @@ if [ "$1" == "cleanup" ]; then
     exit 0;
 fi
 
+if [ "$1" == "help" ]; then 
+    usage
+    exit 0
+fi
+
 echo "Creating front-end service"
 docker service create \
        --publish 8079 \
@@ -52,15 +53,6 @@ docker service create \
        --name front-end --env "reschedule=on-node-failure" weaveworksdemos/front-end:latest
 
 exit_on_failure
-
-# echo "Creating edge-router service"
-# docker service create \
-#        --name edge-router \
-#        --network ingress \
-#        --publish 80 \
-#        --env "reschedule=on-node-failure" weaveworksdemos/edge-router:latest
-
-# exit_on_failure
 
 echo "Creating catalogue service"
 docker service create \
@@ -122,22 +114,6 @@ docker service create \
        --env "reschedule=on-node-failure" weaveworksdemos/shipping:latest
 exit_on_failure
 
-# echo "Creating queue-master service"
-# docker service create \
-#        --name queue-master \
-#        --network ingress \
-#        --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-#        --env "reschedule=on-node-failure" weaveworksdemos/queue-master:latest
-# exit_on_failure
-
-
-# echo "Creating rabbitmq service"
-# docker service create \
-#        --name rabbitmq \
-#        --network ingress \
-#        --env "reschedule=on-node-failure" rabbitmq:3
-# exit_on_failure
-
 echo "Creating payment service"
 docker service create \
        --name payment \
@@ -151,13 +127,3 @@ docker service create \
        --network ingress \
        --env "reschedule=on-node-failure" weaveworksdemos/login:latest
 exit_on_failure
-
-
-
-echo "Creating weave scope service"
-# docker service create \
-#        --name weave-scope \
-#        --network ingress weaveworks/scope:0.15
-exit_on_failure
-
-echo "Done"
