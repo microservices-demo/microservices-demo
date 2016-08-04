7dc501b8556e66529d528fe40204c7d3fb8ecf09
Phil Winder <phil@winderresearch.com>
2016-08-04 11:50:59 +0100

    Make the service/domain to proxy a parameter.

--- sockshop/edge-router/start-nginx.sh
+++ sockshop/edge-router/start-nginx.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-if [ "$NAMESERVER" == "" ]; then
+if [ "$NAMESERVER" == "" ] ; then
 	export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
 fi
 
 echo "Nameserver is: $NAMESERVER"
 
+if [ "$SERVICE" == "" ] ; then
+    export SERVICE=front-end:8079
+fi
+
+echo "Service to proxy is: $SERVICE"
+
 echo "Copying nginx config"
-envsubst '$NAMESERVER' < nginx.conf.template > /etc/nginx.conf
+MYVARS='$NAMESERVER:$SERVICE'
+envsubst "$MYVARS" < nginx.conf.template > /etc/nginx.conf
 
 echo "Using nginx config:"
 echo "-------------------"
