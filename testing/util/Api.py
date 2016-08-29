eef954b7f3393e8fd22947f32568a16dd81e6b1f
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-08-29 09:57:10 +0200

    Remove testing; tidy up travis

--- testing/util/Api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class Api:
-    def noResponse(self, url):
-        try:
-            r = requests.get(url, timeout=5)
-        except requests.exceptions.ConnectionError:
-            return True
-        return r.status_code > 299
