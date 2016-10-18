a7774d1a738037ec595478e502fc21bdf04a89e3
Thijs Schnitger <thijs.schnitger@container-solutions.com>
2016-10-18 15:08:52 +0200

    blocks should be a list

--- staging/main.tf
+++ staging/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "microservices-demo-staging-k8s" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "${var.bastion_cidr_block}"
+    cidr_blocks = ["${var.bastion_cidr_block}"]
   }
   ingress {
     from_port       = 0
