3bcb22c4d63c4b894f992d989b58e8c51a547ea0
Jason Smith <jasonrichardsmith@gmail.com>
2017-01-06 19:19:13 +0100

    added ingress for zipkin

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -29,6 +29,12 @@ resource "aws_security_group" "k8s-security-group" {
     protocol     = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 9411
+    to_port     = 9411
+    protocol     = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
