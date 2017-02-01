4c1046c2fc8b411054b60b306886f7041cfdada6
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2017-02-01 15:52:37 +0200

    changed names and tags of the k8s resources, so its obvious they are part of the sock shop ci

--- deploy/kubernetes/terraform/outputs.tf
+++ deploy/kubernetes/terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "node_addresses" {
-  value = ["${aws_instance.md-k8s-node.*.public_dns}"]
+  value = ["${aws_instance.ci-sockshop-k8s-node.*.public_dns}"]
 }
 
 output "master_address" {
-  value = "${aws_instance.md-k8s-master.public_dns}"
+  value = "${aws_instance.ci-sockshop-k8s-master.public_dns}"
 }
 
 output "sock_shop_address" {
-  value = "${aws_elb.elb-sock-shop.dns_name}"
+  value = "${aws_elb.ci-sockshop-k8s-elb.dns_name}"
 }
