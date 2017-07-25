d1b35169620a29f0802de6b49ba28057b21b88e6
Jason Smith <jasonrichardsmith@gmail.com>
2017-07-25 12:04:43 +0200

    changed import flag back for consistency

--- deploy/docker-compose/grafana/import.sh
+++ deploy/docker-compose/grafana/import.sh
@@ -8,7 +8,8 @@ for file in *-datasource.json; do
     curl --silent --fail --show-error \
       --request POST http://admin:foobar@grafana:3000/api/datasources \
       --header "Content-Type: application/json" \
-      -d "@$file";
+      --header "Accept: application/json" \
+      --data-binary "@$file";
     echo "";
   fi
 done;
@@ -20,7 +21,7 @@ for file in *-dashboard.json; do
     curl --request POST http://admin:foobar@grafana:3000/api/dashboards/import \
       --header "Content-Type: application/json" \
       --header "Accept: application/json" \
-      -d "@$file";
+      --data-binary "@$file";
     echo "";
   fi
 done;
