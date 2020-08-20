225e2df1aa15831b444d1c4302826b1c40a74bb6
y_uuki <yuki.tsubo@gmail.com>
2020-08-20 10:45:08 +0900

    [script] Add a new script for experiment based on events definition file

---
+++ scripts/get_metrics_from_events.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import subprocess
+import sys
+
+def log(msg):
+    print(msg, file=sys.stderr)
+
+def die(msg):
+    print(msg, file=sys.stderr)
+    exit(-1)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("jsonfile", help="Events json file")
+    parser.add_argument("--event", help="Events name")
+    args = parser.parse_args()
+
+    jsonfile = args.jsonfile
+    f = open(jsonfile, 'r') if jsonfile is not None else sys.stdin
+    events = json.load(f)
+
+    if args.event not in events["events"]:
+        die(f"{args.event} not found in {jsonfile}")
+
+    event = events["events"][args.event]
+    params = sum([[f"--{k}", v] for k, v in event['params'].items()], [])
+    cmds = ["./get_metrics_from_prom.py"] + params
+    log(f"Running {' '.join(cmds)}...")
+    subprocess.run(cmds)
+
+if __name__ == '__main__':
+    main()
