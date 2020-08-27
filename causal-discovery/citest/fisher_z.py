d3347e8abeedb0e73cb680e1a136738acbe1ef80
tsurubee <29603512+tsurubee@users.noreply.github.com>
2020-08-27 09:46:37 +0900

    [causal] Rename prepocess.ipynb to dimension-reduction.ipynb

--- causal-discovery/citest/fisher_z.py
+++ causal-discovery/citest/fisher_z.py
@@ -5,7 +5,6 @@ def ci_test_fisher_z(data_matrix, x, y, s, **kwargs):
     assert 'corr_matrix' in kwargs
     cm = kwargs['corr_matrix']
     n = data_matrix.shape[0]
-
     z = zstat(x, y, list(s), cm, n)
     p_val = 2.0 * norm.sf(np.absolute(z))
     return p_val
