ceea2f3976638ec2be57de079490c77221d6fbc0
y_uuki <yuki.tsubo@gmail.com>
2020-09-08 16:17:34 +0900

    [experiment] Add a test script for dimension reduction evaluation

---
+++ causal-discovery/experiments/run_dimension_reduction.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import multiprocessing
+import subprocess
+import sys
+
+CUR_DIR = os.fspath(os.path.dirname(__file__))
+DATA_DIR = f"{CUR_DIR}/../../data/"
+INPUT = {
+    'user-db': {
+        'cpu-load': f"{DATA_DIR}/20200831_user-db_cpu-load_02.json",
+        'network-latency': f"{DATA_DIR}/20200901_user-db_network-latency_01.json",
+    },
+    'shipping': {
+        'cpu-load': f"{DATA_DIR}/20200902_shipping_cpu-load_01.json",
+        'network-latency': f"{DATA_DIR}/20200902_shipping_network-latency_01.json",
+    },
+}
+
+def log(msg):
+    print(msg, file=sys.stderr)
+
+def run(method, num_test, num_cores, output):
+    for container, entries in INPUT.items():
+        for anomaly, inputfile in entries.items():
+            log(f"Running {method} test in case of {container} {anomaly} {inputfile} ...")
+
+            before_num_metrics, filtered_num_metrics, last_num_metrics = 0, 0, 0
+            for i in range(1, args.num_test+1):
+                log(f"Running {method} test in case of {container} {anomaly} {inputfile}: test:{str(i)} ...")
+
+                cmdout = subprocess.Popen(f"{CUR_DIR}/../{method}.py --max-workers {num_cores} {inputfile}",
+                    shell=True, stdout=subprocess.PIPE)
+                jsonS, _ = cmdout.communicate()
+                res = json.loads(jsonS)['metrics_dimension']
+
+                before_num_metrics += res['total'][0]
+                filtered_num_metrics += res['total'][1]
+                last_num_metrics += res['total'][2]
+
+            output[method]['reduction'][container].setdefault({})
+            output[method]['reduction'][container][anomaly].setdefault({})
+            output[method]['reduction'][container][anomaly]['before_num_metrics'] = before_num_metrics / args.num_test
+            output[method]['reduction'][container][anomaly]['filtered_num_metrics'] = filtered_num_metrics / args.num_test
+            output[method]['reduction'][container][anomaly]['last_num_metrics'] = last_num_metrics / args.num_test
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--method", help="'tsifter' or 'sieve'", default='all')
+    parser.add_argument("--num-cores", help="number of CPU cores",
+        type=int, default=multiprocessing.cpu_count())
+    parser.add_argument("--num-test", help="number of test", type=int, default=5)
+    args = parser.parse_args()
+
+    output = {
+        'tsifter': {
+            'reduction': {},
+        },
+        'sieve': {
+            'reduction': {},
+        }
+    }
+
+    if args.method in ['all', 'tsifter']:
+        run('tsifter', args.num_test, args.num_cores, output)
+    if args.method in ['all', 'sieve']:
+        run('sieve', args.num_test, args.num_cores, output)
+
+    json.dump(output, sys.stdout, indent=4)
