ac4b17faadcf193e88e8096299c303236f7ebed1
y_uuki <yuki.tsubo@gmail.com>
2020-09-09 16:07:53 +0900

    Replace re match with string match

--- causal-discovery/tsifter.py
+++ causal-discovery/tsifter.py
@@ -87,22 +87,22 @@ def z_normalization(data):
 
 def count_metrics(metrics_dimension, dataframe, n):
     for col in dataframe.columns:
-        if re.match("^c-", col):
+        if col.startswith("c-"):
             container_name = col.split("_")[0].replace("c-", "")
             if container_name not in metrics_dimension["containers"]:
                 metrics_dimension["containers"][container_name] = [0, 0, 0]
             metrics_dimension["containers"][container_name][n] += 1
-        elif re.match("^m-", col):
+        elif col.startswith("m-"):
             middleware_name = col.split("_")[0].replace("m-", "")
             if middleware_name not in metrics_dimension["middlewares"]:
                 metrics_dimension["middlewares"][middleware_name] = [0, 0, 0]
             metrics_dimension["middlewares"][middleware_name][n] += 1
-        elif re.match("^s-", col):
+        elif col.startswith("s-"):
             service_name = col.split("_")[0].replace("s-", "")
             if service_name not in metrics_dimension["services"]:
                 metrics_dimension["services"][service_name] = [0, 0, 0]
             metrics_dimension["services"][service_name][n] += 1
-        elif re.match("^n-", col):
+        elif col.startswith("n-"):
             node_name = col.split("_")[0].replace("n-", "")
             if node_name not in metrics_dimension["nodes"]:
                 metrics_dimension["nodes"][node_name] = [0, 0, 0]
