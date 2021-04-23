204e2aa61d84f7da6fb4566c06168e5da191f0de
elieser pereira <elieser.pereiraa@gmail.com>
2021-04-23 15:54:46 -0300

    move outdated deployment methods to user-contribs repo

--- deploy/docker-swarm/infra/aws/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "node_addresses" {
-  value = ["${aws_instance.ci-sockshop-docker-swarm-node.*.public_ip}"]
-}
-
-output "master_address" {
-  value = "${aws_instance.ci-sockshop-docker-swarm-master.public_ip}"
-}
