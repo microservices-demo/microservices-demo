b10ea001653872af09b2d71b413ab9f1fb79b862
Magnus <nordbom@live.no>
2023-02-27 17:45:33 +0100

    Working multiIndex conversion!

--- analysis/convertcsv/output.ts
+++ analysis/convertcsv/output.ts
@@ -2,9 +2,9 @@
 @timeStamps false
 @missing false
 @univariate false
-@dimensions 2
+@dimensions 3
 @equalLength true
 @seriesLength 8
 @classLabel false
 @data
-312:313
+312,345,355,355,356,354,355,356:0,0,0,0,1,1,1,2:45,87,23,67,12,3,654,4
