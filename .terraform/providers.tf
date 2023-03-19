e6db761f13004f71a0eaa457aecc0b10ac1cfa50
Jadesola01 <iyowujadesola@gmail.com>
2023-03-19 15:12:25 +0100

    terraform folder to provision eks

---
+++ .terraform/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  cloud {
+    workspaces {
+      name = "learn-terraform-eks"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
