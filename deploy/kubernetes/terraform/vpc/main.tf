3c8560d6b6c36fe03452bd947ff589a237660009
Matic, Andrija <matica@ae.com>
2023-04-27 10:21:48 +0200

    Created VPC, internet gateway, public and private subnetworks, it's route tables and it's associations

--- deploy/kubernetes/terraform/vpc/main.tf
+++ deploy/kubernetes/terraform/vpc/main.tf
@@ -1,26 +1,27 @@
 provider "aws" {
   region = var.region
 }
+
 resource "aws_vpc" "vpc_main_amatic" {
   cidr_block = var.vpc_cidr_block
-  enable_dns_hostnames = true
+  #enable_dns_hostnames = true
 
-  tags {
+  tags =  {
     Name = "vpc_main_amatic"
   }
 }
 resource "aws_internet_gateway" "gateway_amatic" {
-  vpc_id = "${aws_vpc.vpc_main_amatic.id}"
-  tags {
+  vpc_id = aws_vpc.vpc_main_amatic.id
+  tags =  {
     Name = "gateway_matic"
   }
 }
 
 // Public subnetwork it's route table and it's association
 resource "aws_subnet" "public_subnetwork_amatic" {
-  vpc_id = "${aws_vpc.vpc_main_amatic.id}"
-  cidr_block = var.subnetwork_cidr_block
-  tags {
+  vpc_id = aws_vpc.vpc_main_amatic.id
+  cidr_block = var.public_subnetwork_cidr_block
+  tags = {
     Name = "public_subnetwork_amatic"
   }
   availability_zone = var.availability_zone
@@ -28,28 +29,28 @@ resource "aws_subnet" "public_subnetwork_amatic" {
   map_public_ip_on_launch = true
 }
 resource "aws_route_table" "public_subnetwork_route_table_amatic" {
-  vpc_id = "${aws_vpc.vpc_main_amatic.id}"
+  vpc_id = aws_vpc.vpc_main_amatic.id
 
   route {
     cidr_block = var.route_table_cidr_block
-    gateway_id = "${aws_internet_gateway.gateway_amatic.id}"
+    gateway_id = aws_internet_gateway.gateway_amatic.id
   }
 
-  tags {
+  tags  = {
     Name = "public_subnetwork_route_table_amatic"
   }
 }
 resource "aws_route_table_association" "public_route_table_association_amatic" {
-  subnet_id = "${aws_subnet.vpc_main_amatic.id}"
-  route_table_id = "${aws_route_table.public_subnetwork_route_table_amatic.id}"
+  subnet_id = aws_subnet.public_subnetwork_amatic.id
+  route_table_id = aws_route_table.public_subnetwork_route_table_amatic.id
 }
 
 
 // Private subnetwork it's route table and it's association
 resource "aws_subnet" "private_subnetwork_amatic" {
-  vpc_id = "${aws_vpc.vpc_main_amatic.id}"
-  cidr_block = var.subnetwork_cidr_block
-  tags {
+  vpc_id = aws_vpc.vpc_main_amatic.id
+  cidr_block = var.private_subnetwork_cidr_block
+  tags =  {
     Name = "private_subnetwork_amatic"
   }
   availability_zone = var.availability_zone
@@ -57,18 +58,18 @@ resource "aws_subnet" "private_subnetwork_amatic" {
   map_public_ip_on_launch = true
 }
 resource "aws_route_table" "private_subnetwork_route_table_amatic" {
-  vpc_id = "${aws_vpc.vpc_main_amatic.id}"
+  vpc_id = aws_vpc.vpc_main_amatic.id
 
   route {
     cidr_block = var.route_table_cidr_block
-    gateway_id = "${aws_internet_gateway.gateway_amatic.id}"
+    gateway_id = aws_internet_gateway.gateway_amatic.id
   }
 
-  tags {
+  tags = {
     Name = "private_subnetwork_route_table_amatic"
   }
 }
 resource "aws_route_table_association" "private_route_table_association_amatic" {
-  subnet_id = "${aws_subnet.vpc_main_amatic.id}"
-  route_table_id = "${aws_route_table.private_subnetwork_route_table_amatic.id}"
+  subnet_id = aws_subnet.private_subnetwork_amatic.id
+  route_table_id = aws_route_table.private_subnetwork_route_table_amatic.id
 }
--- deploy/kubernetes/terraform/vpc/variables.tf
+++ deploy/kubernetes/terraform/vpc/variables.tf
@@ -1,17 +1,20 @@
 variable "region" {
-  default = "us-west-1"
+  default = "eu-central-1"
 }
 
 variable "availability_zone" {
-  default = "us-west-1b"
+  default = "eu-central-1a"
 }
 
 variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 
-variable "subnetwork_cidr_block" {
-  default = "10.0.0.0/24"
+variable "public_subnetwork_cidr_block" {
+  default = "10.0.1.0/24"
+}
+variable "private_subnetwork_cidr_block" {
+  default = "10.0.2.0/24"
 }
 
 variable "route_table_cidr_block" {
