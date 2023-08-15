ae95683d0a66dcf2a0776950db600a1255ea28c0
Nikola <nikola.kuzman@quietplatforms.com>
2023-08-15 11:50:49 +0200

    Creating infra and pipelines

---
+++ infra/main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "nkuzman-vpc"
+}
+
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = var.public_subnet
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.name
+
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = var.private_subnet
+  ip_cidr_range = "10.0.2.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.name
+
+  private_ip_google_access = true
+}
+
+resource "google_container_cluster" "my_cluster" {
+  name     = "nkuzman"
+  location = var.region
+  project  = var.project_id
+
+  node_pool {
+    name       = "default-node-pool"
+    initial_node_count = 1
+    node_count = 1
+
+    node_config {
+      machine_type = "n1-standard-2"
+    }
+  }
+}
